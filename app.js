be2bd91dfcd89b1c68a34cc5b2529332298a5824
Jian Li <lj2312763@163.com>
2017-03-13 14:57:04 +0800

    提交服务器app

---
+++ app.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var pgk=require('./package.json');
+var host=pgk.config.host;
+var port =pgk.config.port;
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+//静态文件地址JavaScript、image、css……
+app.use(express.static(path.join(__dirname, 'public')));
+
+var count = 0;
+//放在虚拟路径，在访问时要在端口后添加虚拟的路径
+//app.use('/static', express.static('public'));
+
+//路由设定
+app.use('/', index);
+app.use('/users', users);
+
+app.get('/api/w', function (req, res, next) {
+  console.log(req.route.path);
+  console.log(++count);
+  //next('/p');
+  var result={
+    errcode:1,
+    data:'ww1w15',
+    message:''
+  };
+  res.send(result);
+  //next('/');
+});
+
+//所有请求都会执行，不分get、post、put等等
+app.use(function (req, res, next) {
+  console.log('loading……');
+  console.log(++count);
+  //res.status(200).send({errcode:1,message:'服务器错误'})
+  //res.status(200).sendFile('/问题单#41242-1.jpg');
+  //next();
+});
+
+app.use(function(req,res,next){
+  console.log(`time:${Date.now()}`);
+});
+
+// catch 404 and forward to error handler
+//捕获404异常
+app.use(function (req, res, next) {
+  console.log(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development'
+      ? err
+      : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+var server = app.listen(port,host, function () {
+  //var host = server.address().address;
+  //var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+module.exports = app;
